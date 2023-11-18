900dd0d569a8cf7279463eebae47c72c5fa02121
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2023-11-19 00:24:25 +0100

    Fix enums export

--- src/kamereon/index.ts
+++ src/kamereon/index.ts
@@ -1,3 +1,4 @@
 export type * from './models';
+export { PlugStatus, ChargingStatus } from './models/battery-status-enum.model';
 export * from './kamereon.constants';
 export { KamereonApi } from './kamereon.api';
